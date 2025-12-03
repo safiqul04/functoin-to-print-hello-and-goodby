881e7c8f901eef8d878db16d5457856bb4a92a10
Safiqul Islam <safiqulislam0413@gmail.com>
2025-12-03 20:57:22 +0530

    Add files via upload

---
+++ function_Hello-Goodby.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void printHello();
+void printGoodby();
+int main(){
+    printHello();
+    printGoodby();
+    return 0;
+}
+void printHello(){
+    printf("Hello\n");
+}
+void printGoodby(){
+    printf("Good By\n");
+}
